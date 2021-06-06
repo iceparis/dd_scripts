1ad1b003e02687c08dfd7fe780da9cb0d3d33059
iceparis <80054093+iceparis@users.noreply.github.com>
2021-06-06 23:12:19 +0800

    Update shell_script_mod.sh

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -32,17 +32,17 @@ function Wenmoux(){
 }
 
 function redrain(){
-    # https://github.com/nianyuguai/longzhuzhu
+    # https://github.com/longzhuzhu/nianyu
     rm -rf /longzhuzhu
     rm jd_half_redrain.js
     rm jd_super_redrain.js
     rm long_hby_lottery.js
-    git clone https://github.com/iceparis/longzhuzhu.git /longzhuzhu
+    git clone https://github.com/longzhuzhu/nianyu.git /longzhuzhu
     # 拷贝脚本
     for jsname in $(find /longzhuzhu/qx -name "*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
-    echo "30 16-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    echo "0 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    echo "1 17-23/1 1-18 6 * node /scripts/long_hby_lottery.js >> /scripts/logs/long_hby_lottery.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "30 16-23/1 * * * node /scripts/long_half_redrain.js >> /scripts/logs/long_half_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "0 0-23/1 * * * node /scripts/long_super_redrain.js >> /scripts/logs/long_super_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "1 20 1-18 6 * node /scripts/long_hby_lottery.js >> /scripts/logs/long_hby_lottery.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 
 function main(){
