b000cfea8050380d0146e1145cc0b6e3639c5154
iceparis <80054093+iceparis@users.noreply.github.com>
2021-06-06 23:35:09 +0800

    Update shell_script_mod.sh

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 ## CUSTOM_SHELL_FILE for https://gitee.com/lxk0301/jd_docker/tree/master/docker
-### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/monk-coder/dust/dust/shell_script_mod.sh
+### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/iceparis/dd_scripts/sync/jdshell/shell_script_mod.sh
 #### 容器完全启动后执行 docker exec -it jd_scripts /bin/sh -c 'crontab -l'
-
-function monkcoder(){
-    # https://github.com/monk-coder/dust
-    rm -rf /monkcoder /scripts/monkcoder_*
-    git clone https://github.com/iceparis/monk-dust.git /monkcoder
-    # 拷贝脚本https://github.com/monk-coder/dust.git
-    for jsname in $(find /monkcoder/normal -name "*.js"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
-    # 匹配js脚本中的cron设置定时任务
-    for jsname in $(find /monkcoder/normal -name "*.js"); do
-        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
-        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    done
-}
 
 function Wenmoux(){
     # https://github.com/Wenmoux/scripts
@@ -45,7 +32,6 @@ function redrain(){
 function main(){
     # 首次运行时拷贝docker目录下文件
     [[ ! -d /jd_diy ]] && mkdir /jd_diy && cp -rf /scripts/docker/* /jd_diy
-    monkcoder
     Wenmoux
     redrain
     # 拷贝docker目录下文件供下次更新时对比
