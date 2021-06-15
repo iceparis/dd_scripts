9e83ca74beae9cde50f11025f5a551a50b2991ba
iceparis <80054093+iceparis@users.noreply.github.com>
2021-06-15 11:44:17 +0800

    Update shell_script_mod.sh

--- myshell/shell_script_mod.sh
+++ myshell/shell_script_mod.sh
@@ -24,7 +24,7 @@ function jddj(){
     # 下载cookie文件失败时从备份恢复
     test $? -eq 0 || cp -rf /scripts/jddj/backup_jddj_cookie.js /scripts/backup_jddj_cookie.js
     # 拷贝脚本
-    for jsname in $(find /jddj -name "jddj_fruit*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
+    for jsname in $(find /scripts/jddj -name "jddj_*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
     # 设定任务
     echo "10 0,3,8,11,17 * * * node /scripts/jddj/jddj_fruit.js >> /scripts/logs/jddj_fruit.log 2>&1" >> /scripts/docker/merged_list_file.sh
     echo "*/5 * * * * node /scripts/jddj/jddj_fruit_collectWater.js >> /scripts/logs/jddj_fruit_collectWater.log 2>&1" >> /scripts/docker/merged_list_file.sh
