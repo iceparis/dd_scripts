18eb471d467a37b7319b20842692bd6ffa7a31d9
iceparis <80054093+iceparis@users.noreply.github.com>
2021-07-23 21:13:06 +0800

    Update shell_script_mod.sh

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -19,6 +19,7 @@ function smiek(){
     # 拷贝脚本
     for jsname in $(find /smiek -name "*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
     echo "10 10 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    echo "35 0,6-23/2 * * * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 function main(){
     # 首次运行时拷贝docker目录下文件
