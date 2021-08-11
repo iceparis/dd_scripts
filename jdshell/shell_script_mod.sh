9ac6766e733038cc5f00c4010dd5b8deffa3dfca
iceparis <80054093+iceparis@users.noreply.github.com>
2021-08-11 19:38:34 +0800

    update

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -3,15 +3,6 @@
 ### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/iceparis/dd_scripts/sync/jdshell/shell_script_mod.sh
 #### 容器完全启动后执行 docker exec -it jd_scripts /bin/sh -c 'crontab -l'
 
-function redrain(){
-    # https://github.com/longzhuzhu/nianyu
-    rm -rf /longzhuzhu
-    git clone https://ghproxy.com/https://github.com/iceparis/nianyu.git /longzhuzhu
-    # 拷贝脚本
-    for jsname in $(find /longzhuzhu/qx -name "*.js"); do cp ${jsname} /scripts/${jsname##*/}; done
-    echo "30 20-23/1 * * * node /scripts/long_half_redrain.js >> /scripts/logs/long_half_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    echo "0 0-23/1 * * * node /scripts/long_super_redrain.js >> /scripts/logs/long_super_redrain.log 2>&1" >> /scripts/docker/merged_list_file.sh
-}
 function smiek(){
     # https://github.com/smiek2221/scripts
     rm -rf /smiek
@@ -25,7 +16,6 @@ function smiek(){
 function main(){
     # 首次运行时拷贝docker目录下文件
     [[ ! -d /jd_diy ]] && mkdir /jd_diy && cp -rf /scripts/docker/* /jd_diy
-    redrain
     smiek
     # 拷贝docker目录下文件供下次更新时对比
     cp -rf /scripts/docker/* /jd_diy
