96b67de97b2e3d664fd2f30214dfdfbac95c977d
iceparis <80054093+iceparis@users.noreply.github.com>
2021-09-17 12:10:35 +0800

    Update shell_script_mod.sh

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -24,7 +24,7 @@ function star261(){
     fi
     cp -f /star261/scripts/jd_jxmc.js /scripts
     cp -f /star261/scripts/jd_productZ4Brand.js /scripts
-     echo "5 9 * * * node /scripts/jd_productZ4Brand.js >> /scripts/logs/jd_productZ4Brand.log 2>&1" >> /scripts/docker/merged_list_file.sh
+     echo "8 1 * * * node /scripts/jd_productZ4Brand.js >> /scripts/logs/jd_productZ4Brand.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 function main(){
     # 首次运行时拷贝docker目录下文件
