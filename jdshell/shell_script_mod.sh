c144c02131a0551b41e79d779057e40bd4b38e57
iceparis <80054093+iceparis@users.noreply.github.com>
2021-10-31 14:49:57 +0800

    Update shell_script_mod.sh

--- jdshell/shell_script_mod.sh
+++ jdshell/shell_script_mod.sh
@@ -3,16 +3,6 @@
 ### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/iceparis/dd_scripts/sync/jdshell/shell_script_mod.sh
 #### 容器完全启动后执行 docker exec -it jd_scripts /bin/sh -c 'crontab -l'
 
-function smiek(){
-    # https://github.com/smiek2221/scripts
-    rm -rf /smiek
-    git clone https://github.com/smiek2221/scripts.git /smiek
-    # 拷贝脚本
-    cp /smiek/JDJRValidator_Pure.js /scripts/JDJRValidator_Pure.js
-    cp /smiek/jd_sign_graphics.js /scripts/jd_sign_graphics.js
-    cp /smiek/sign_graphics_validate.js /scripts/sign_graphics_validate.js
-    echo "6 0,1 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1" >> /scripts/docker/merged_list_file.sh
-}
 function star261(){
      if [ ! -d "/star261/" ]; then
         echo "未检查到star261仓库脚本，初始化下载相关脚本..."
@@ -31,7 +21,6 @@ function star261(){
 function main(){
     # 首次运行时拷贝docker目录下文件
     [[ ! -d /jd_diy ]] && mkdir /jd_diy && cp -rf /scripts/docker/* /jd_diy
-    smiek
     star261
     # 拷贝docker目录下文件供下次更新时对比
     cp -rf /scripts/docker/* /jd_diy
